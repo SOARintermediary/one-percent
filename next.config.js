aca87125d9b05d734bc573038c939c2822c28aa3
B <barney.sykes@soarbusinessgroup,com>
2025-05-31 19:35:10 +0400

    Fix image domains and upgrade Next.js/PostCSS for security

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
+  images: {
+    domains: ['images.pexels.com'],
   },
-  images: { unoptimized: true },
 };
-
 module.exports = nextConfig;
